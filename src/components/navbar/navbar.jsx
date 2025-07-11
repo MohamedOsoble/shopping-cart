b4ae828dab62b009a25437764853e95d7e84a9f6
Mohamed Osoble <mohamedosoble@outlook.com>
2025-07-11 20:16:37 +0100

    fix: navbar styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,31 +5,15 @@ import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.section}>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to="shop">Shop</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-right">
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <div className={styles.section}>
-              <input type="text" placeholder="Search..."></input>
-              <button type="submit" className={styles.button}>
-                Search
-              </button>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <Link to="cart">Cart</Link>
-          </li>
-        </ul>
-      </div>
+      <Link className={styles.link} to="/">
+        Home
+      </Link>
+      <Link className={styles.link} to="shop">
+        Shop
+      </Link>
+      <Link className={styles.link} to="cart">
+        Cart
+      </Link>
     </nav>
   );
 };
